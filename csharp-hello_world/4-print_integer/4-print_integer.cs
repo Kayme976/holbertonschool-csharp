6ae2c5bd13c0479a070f7f11d25b8e688ec2844a
Kayme976 <7508@holbertonstudents.com>
2024-12-06 16:24:10 +0100

    new4

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,10 @@
-using system;
+using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        int number = 972;
-        Console.WriteLine(string.Format("{0} Mission Street", number));
-    }
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine(string.Format("{0} Mission Street", number));
+	}
 }
