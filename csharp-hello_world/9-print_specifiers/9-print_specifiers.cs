f25f7ac137877b03d18bdce15facce5a17f242f0
Kayme976 <7508@holbertonstudents.com>
2024-12-06 15:09:05 +0100

    last correction

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,7 +4,7 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		double percent = .7553;
+		double percent = 0.7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:0.00%}", percent);
         Console.WriteLine("Currency: {0:C2}", currency);
