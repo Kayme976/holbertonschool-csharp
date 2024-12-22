f3ffbdc205c0e7f3e85b32f3979b6692c8a1ccb2
Kayme976 <7508@holbertonstudents.com>
2024-12-23 00:16:11 +0100

    very last comit

--- csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 class Dictionary
 {
-    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key){
-    }
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
         myDict.Remove(key);
         return myDict;
     }
